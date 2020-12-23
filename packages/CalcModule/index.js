1f07041c526f2c15361cc7193f9224b88841d589
jamalmohamad <mohammed.jamalhadi@gmail.com>
2020-12-23 20:51:47 +0100

    initial setup

---
+++ packages/CalcModule/index.js
@@ -0,0 +1,5 @@
+var additionModule = require("AdditionModule");
+var subtractionModule = require("SubtractionModule");
+
+console.log("Add 4 + 5 = " + additionModule.add(4,5));
+console.log("Add 5 + 5 = " + subtractionModule.subtract(5,5));
